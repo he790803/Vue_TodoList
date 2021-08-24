06bf06bd050c568a4a80fb4ef44ee131ad5f45c5
Henry <he790803@gmail.com>
2021-08-24 15:12:52 +0800

    新增，編輯，刪除功能

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,7 @@ export default new Vuex.Store({
     SET_TODOS (state, todos) {
       state.todos = todos
     }
+
   },
   // 呼叫mutations=>使用commit
   actions: {
@@ -27,14 +28,42 @@ export default new Vuex.Store({
         todos
       }
     },
-    CREATE_TODO ({ commit }) {
-
+    CREATE_TODO ({ commit }, { todo }) {
+      // 1. POST // 使用API的情況:axios.post()
+      const todos = STORE.load()
+      todos.push(todo)
+      STORE.save(todos)
+      // 2. commit 呼叫mutations
+      commit('SET_TODOS', todos)
+      // 3. return
+      return {
+        tid: todos.length - 1,
+        todo
+      }
     },
-    EIDE_TODO ({ commit }, { todos, tid }) {
-
+    EIDE_TODO ({ commit }, { tid, todo }) {
+      // 1. 使用API的情況:axios.delete()
+      const todos = STORE.load()
+      const newTodo = todos.splice(tid, 1, todo)
+      STORE.save(todos)
+      // 2. commit 呼叫mutations
+      commit('SET_TODOS', todos)
+      return {
+        tid,
+        newTodo
+      }
     },
     DELETE_TODO ({ commit }, { tid }) {
-
+      // 1. 使用API的情況:axios.delete()
+      const todos = STORE.load()
+      const todo = todos.splice(tid, 1)[0]
+      STORE.save(todos)
+      // 2. commit 呼叫mutations
+      commit('SET_TODOS', todos)
+      return {
+        tid: null,
+        todo
+      }
     }
   },
   modules: {}
